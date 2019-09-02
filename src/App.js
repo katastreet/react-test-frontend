e984fbde5d983c457af64d9e634a53708af52d82
katastreet <satyarth7a@hotmail.com>
2019-09-02 23:29:37 +0545

    React based front end for django backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
+import Nav from './components/Nav';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayed_form: '',
+      logged_in: localStorage.getItem('token') ? true : false,
+      username: ''
+    };
+  }
+
+  componentDidMount() {
+    if (this.state.logged_in) {
+      fetch('http://localhost:8000/hello', {
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('token')}`
+        }
+      })
+          .then(res => {
+            if (res === true) {
+              this.setState({username: 'user'})
+            }
+
+          })
+
+    }
+  }
+
+  handle_login = (e, data) => {
+    e.preventDefault();
+    console.log('this', data);
+    fetch('http://localhost:8000/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+        .then(res => res.json())
+        .then(json => {
+          console.log(json);
+          localStorage.setItem('token', json.access);
+          this.setState({
+            logged_in: true,
+            displayed_form: '',
+            username: 'user'
+          });
+        });
+  };
+
+
+  handle_logout = () => {
+    localStorage.removeItem('token');
+    this.setState({ logged_in: false, username: '' });
+  };
+
+  display_form = form => {
+    this.setState({
+      displayed_form: form
+    });
+  };
+
+  render() {
+    return (
+        <div className="App">
+          <Nav
+              logged_in={this.state.logged_in}
+              display_form={this.display_form}
+              handle_logout={this.handle_logout}
+          />
+          {this.state.logged_in? '': <LoginForm handle_login={this.handle_login} />}
+
+          <h3>
+            {this.state.logged_in
+                ? `Hello, you are logged in`
+                : ''}
+          </h3>
+        </div>
+    );
+  }
 }
 
 export default App;
